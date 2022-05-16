2eda79ac72db3999ba660b687d793a05b25344fb
kalkidan12 <kalget12@gmail.com>
2022-05-16 23:54:41 +0300

    centered

--- front-end/src/View/LoginPage.jsx
+++ front-end/src/View/LoginPage.jsx
@@ -1,5 +1,5 @@
 import NavBar from "../Components/NavBar";
-
+import { Box } from "rebass";
 // function LoginPage() {
 // 	return (
 // 		<>
@@ -14,47 +14,55 @@ const LoginPage = () => {
 	return (
 		<>
 			<NavBar />
-			<div className="loginpage">
-				<form>
-					<h3>Sign In</h3>
-					<div className="mb-3">
-						<label>Email address</label>
-						<input
-							type="email"
-							className="form-control"
-							placeholder="Enter email"
-						/>
-					</div>
-					<div className="mb-3">
-						<label>Password</label>
-						<input
-							type="password"
-							className="form-control"
-							placeholder="Enter password"
-						/>
-					</div>
-					<div className="mb-3">
-						<div className="custom-control custom-checkbox">
+			<Box
+				sx={{
+					maxWidth: 512,
+					mx: "auto",
+					px: 3,
+				}}
+			>
+				<div className="loginpage">
+					<form>
+						<h3>Sign In</h3>
+						<div className="mb-3">
+							<label>Email address</label>
 							<input
-								type="checkbox"
-								className="custom-control-input"
-								id="customCheck1"
+								type="email"
+								className="form-control"
+								placeholder="Enter email"
 							/>
-							<label className="custom-control-label" htmlFor="customCheck1">
-								Remember me
-							</label>
 						</div>
-					</div>
-					<div className="d-grid">
-						<button type="submit" className="btn btn-primary">
-							Login
-						</button>
-					</div>
-					<p className="forgot-password text-right">
-						Forgot <a href="#">password?</a>
-					</p>
-				</form>
-			</div>
+						<div className="mb-3">
+							<label>Password</label>
+							<input
+								type="password"
+								className="form-control"
+								placeholder="Enter password"
+							/>
+						</div>
+						<div className="mb-3">
+							<div className="custom-control custom-checkbox">
+								<input
+									type="checkbox"
+									className="custom-control-input"
+									id="customCheck1"
+								/>
+								<label className="custom-control-label" htmlFor="customCheck1">
+									Remember me
+								</label>
+							</div>
+						</div>
+						<div className="d-grid">
+							<button type="submit" className="btn btn-primary">
+								Login
+							</button>
+						</div>
+						<p className="forgot-password text-right">
+							Forgot <a href="#">password?</a>
+						</p>
+					</form>
+				</div>
+			</Box>
 		</>
 	);
 };
